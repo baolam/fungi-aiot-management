4f774764ad28ac016424ff1d69735123caae73c3
baolam <lam01662052827@gmail.com>
2025-05-02 22:27:39 +0700

    `feat: add chat UI kit and implement chat functionality in HarvestAssistant component`

--- src/functions/harvest/HarvestAssistant.jsx
+++ src/functions/harvest/HarvestAssistant.jsx
@@ -1,7 +1,141 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Avatar,
+  TypingIndicator,
+} from '@chatscope/chat-ui-kit-react';
+import { useParams } from 'react-router-dom';
+import botImage from '../../assets/imgs/bot.jpeg';
+import userImage from '../../assets/imgs/user.jpg';
+import { useSocket } from '../../hooks/useSocket';
 
 const HarvestAssistant = () => {
-  return <div>HarvestAssistant</div>;
+  const { id } = useParams();
+  const [messages, setMessages] = useState([
+    {
+      direction: 'incoming',
+      message: 'Tui là trợ lý nông nghiệp',
+      avatar: true,
+    },
+    {
+      direction: 'incoming',
+      message: 'Tui sẽ hỗ trợ bạn giải đáp những thắc mắc về vụ mùa với ID: ' + id,
+    },
+    {
+      direction: 'incoming',
+      message:
+        'Tui được thiết kế với trí nhớ ngắn hạn, bạn hãy dùng tui như một công cụ truy vấn nha',
+    },
+    {
+      direction: 'incoming',
+      message: 'Hỏi thoải mái nhưng lịch sử trò chuyện sẽ không được lưu lại đâu nhé!',
+    },
+  ]);
+
+  const [isBotThinking, setIsBotThinking] = useState(false);
+
+  const socket = useSocket();
+
+  useEffect(() => {
+    socket.on('query', (resp) => {
+      const { harvestId, response } = resp;
+
+      setIsBotThinking(false);
+      if (harvestId === id) {
+        const object = {
+          direction: 'incoming',
+          message: response,
+          avatar: messages[messages.length - 1].direction === 'outgoing',
+        };
+
+        setMessages([...messages, object]);
+      }
+    });
+
+    return () => {
+      socket.off('query');
+    };
+  }, [socket, id, messages]);
+
+  const onSendMessage = (textContent) => {
+    const object = {
+      direction: 'outgoing',
+      message: textContent,
+      avatar: messages[messages.length - 1].direction === 'incoming',
+    };
+
+    const queryInfor = {
+      harvestId: id,
+      query: textContent,
+    };
+
+    setTimeout(() => {
+      socket.emit('query-curious', queryInfor);
+    }, 500);
+
+    setIsBotThinking(true);
+    setMessages([...messages, object]);
+  };
+
+  return (
+    <>
+      <h4 className="text-center">Chat with me!</h4>
+      <div style={{ position: 'relative', height: '400px' }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              typingIndicator={
+                isBotThinking ? <TypingIndicator content="Tui đang nghĩ, đợi xíu..." /> : null
+              }
+            >
+              {messages.map((object, index) => {
+                const sender = object.direction === 'incoming' ? 'Bot' : 'You';
+                if (object.avatar) {
+                  return (
+                    <Message
+                      key={index}
+                      model={{
+                        direction: object.direction,
+                        message: object.message,
+                        sender,
+                      }}
+                    >
+                      <Avatar
+                        name={sender}
+                        src={object.direction === 'incoming' ? botImage : userImage}
+                      />
+                    </Message>
+                  );
+                }
+
+                return (
+                  <Message
+                    key={index}
+                    avatarSpacer
+                    model={{
+                      direction: object.direction,
+                      message: object.message,
+                      sender,
+                    }}
+                  />
+                );
+              })}
+            </MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              // eslint-disable-next-line no-unused-vars
+              onSend={(_, textContent, __, ___) => onSendMessage(textContent)}
+              disabled={isBotThinking}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </>
+  );
 };
 
 export default HarvestAssistant;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import App from './App.jsx';
 import SocketProvider from './providers/SocketProvider.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-circular-progressbar/dist/styles.css';
 import './assets/css/style.css';
